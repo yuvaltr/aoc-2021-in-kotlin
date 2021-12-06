3fd92fd346562ae714f161fbc1ae63c33fe7e4c4
Yuval Traitelis <yuval.traitelis@foretellix.com>
2021-12-06 12:52:11 +0200

    day 2 - dive

---
+++ src/main/kotlin/day02/Day02.kt
@@ -0,0 +1,49 @@
+package day02
+
+import readInput
+
+fun main() {
+    data class Operation(
+        val direction: String,
+        val amount: Int
+    )
+
+    fun part1(input: List<Operation>): Int {
+        var x = 0
+        var y = 0
+        input.forEach {
+            when (it.direction) {
+                "forward" -> x += it.amount
+                "down" -> y += it.amount
+                "up" -> y -= it.amount
+            }
+        }
+        return x * y
+    }
+
+    fun part2(input: List<Operation>): Int {
+        var x = 0
+        var y = 0
+        var aim = 0
+        input.forEach {
+            when (it.direction) {
+                "forward" -> {
+                    x += it.amount
+                    y += aim * it.amount
+                }
+                "down" -> aim += it.amount
+                "up" -> aim -= it.amount
+            }
+        }
+        return x * y
+    }
+
+
+    val testInput = readInput("day02/Day02_test.txt")
+        .map { it.split(' ') }
+        .map { Operation(it[0], it[1].toInt()) }
+
+    check(testInput.size == 1000)
+    println("part1 = ${part1(testInput)}")
+    println("part2 = ${part2(testInput)}")
+}
