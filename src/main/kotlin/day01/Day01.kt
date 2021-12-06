15657455302494b152c092e8061d4e345cb5ad3b
Yuval Traitelis <yuval.traitelis@foretellix.com>
2021-12-06 12:40:44 +0200

    day 1 - sonar sweep

---
+++ src/main/kotlin/day01/Day01.kt
@@ -0,0 +1,29 @@
+package day01
+
+import readInput
+
+fun main() {
+    fun part1(input: List<Int>): Int {
+        val a = input.windowed(2)
+        val b = a.count{ (a, b) -> b > a }
+        return b
+    }
+
+    fun part2(input: List<Int>): Int {
+        val a = input.windowed(4)
+        val b = a.count{ (a, b, c, d) -> d > a }
+        return b
+    }
+
+
+    val testInput = readInput("day01/Day01_test.txt")
+        .map { it.toInt() }
+
+    check(testInput.size == 2000)
+    val res = part1(testInput)
+    println("part1 = ${res}")
+
+    val res2 = part2(testInput)
+    println("part2 = ${res2}")
+
+}
