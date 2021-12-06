33a659e03fb58cb4b01f75a33f7dc4cf9eeadff0
Yuval Traitelis <yuval.traitelis@foretellix.com>
2021-12-06 14:58:11 +0200

    day 3 - binary (pt. 2)

--- src/main/kotlin/day03/Day03.kt
+++ src/main/kotlin/day03/Day03.kt
@@ -4,26 +4,68 @@ import readInput
 import kotlin.math.pow
 
 fun main() {
-    fun part1(input: List<String>): Int {
+    fun stringToBits(s: String): List<Int> {
+        return s.toCharArray().map { c -> c.digitToInt() }
+    }
+
+    fun commonBits(input: List<String>, tieBreaker: Int = 1): List<Int> {
         val bitCount = input
-            .map { it -> it.toCharArray().map{ c -> c - '0'} }
-            .reduce { sum, element -> sum.zip(element){ a, b -> a + b} }
-        
-        val commonBits = bitCount
-            .map { if (it > input.size / 2) 1 else 0 }
+            .map { stringToBits(it) }
+            .reduce { sum, element -> sum.zip(element) { a, b -> a + b } }
+
+        return bitCount
+            .map {
+                if (2 * it >= input.size)
+                    tieBreaker
+                else
+                    1 - tieBreaker
+            }
+    }
+
+    fun bitsToInt(bits: List<Int>): Int {
+        var n = 0
+        bits.reversed().forEachIndexed { index, i -> n += i * 2.0.pow(index.toDouble()).toInt() }
+        return n
+    }
+
+    fun part1(input: List<String>): Int {
+        val commonBits = commonBits(input)
 
         //val gamma1 = commonBits.foldRightIndexed(0) { i, element, sum -> sum + element * 2.0.pow(i.toDouble()).toInt() }
-
-        var gamma = 0
-        commonBits.reversed().forEachIndexed { index, i -> gamma += i * 2.0.pow(index.toDouble()).toInt() }
+        val gamma = bitsToInt(commonBits)
 
         val epsilon = 2.0.pow((commonBits.size).toDouble()).toInt() - 1 - gamma // 2-complement
 
         return gamma * epsilon
     }
 
-    val testInput = readInput("day03/input.txt")
+    fun filterByBit(input: List<String>, index: Int, desiredBit: Int /*Boolean*/): List<String> {
+        return input.filter{ it.toCharArray()[index].digitToInt() == desiredBit }
+    }
 
+    fun filterByCommonBits(input: List<String>, tieBreaker: Int): Int {
+        var filtered = input
+        var index = 0
+        while (filtered.size > 1) {
+            val commonBits = commonBits(filtered, tieBreaker)
+            filtered = filterByBit(filtered, index, commonBits[index])
+            index += 1
+        }
+
+        check(filtered.size == 1)
+        return bitsToInt(stringToBits(filtered[0]))
+    }
+
+    fun part2(input: List<String>): Int {
+        val oxygen = filterByCommonBits(input, tieBreaker = 1)
+        val co2 = filterByCommonBits(input, tieBreaker = 0)
+
+        return oxygen * co2
+    }
+
+    val testInput = readInput("day03/input.txt")
     check(testInput.size == 1000)
+
     println("part1 = ${part1(testInput)}")
+    println("part2 = ${part2(testInput)}")
 }
