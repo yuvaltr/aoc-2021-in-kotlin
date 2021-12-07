a4f2c771f8dc807c4686dfca62708d77f4748160
Yuval Traitelis <yuval.traitelis@foretellix.com>
2021-12-07 21:57:54 +0200

    day 5 - line overlaps (pt. 2)

--- src/main/kotlin/day05/Day05.kt
+++ src/main/kotlin/day05/Day05.kt
@@ -3,6 +3,7 @@ package day05
 import readInput
 import java.lang.Integer.max
 import java.lang.Integer.min
+import java.lang.Math.abs
 
 
 typealias Point = Pair<Int, Int>
@@ -19,34 +20,71 @@ data class Line(
         return start.first == end.first
     }
 
+    fun isDiagonal(): Boolean {
+        return (abs(end.first - start.first) == abs(end.second - start.second))
+    }
+
     fun getLatticePoints(): List<Point> {
         return if (isHorizontal()) {
             val a = min(start.first, end.first)
             val b = max(start.first, end.first)
             (a..b).map{Point(it, start.second)}
+
         } else if (isVertical()) {
             val a = min(start.second, end.second)
             val b = max(start.second, end.second)
-            (a..b).map{Point(start.first, it)}
+            (a..b).map { Point(start.first, it)}
+
+        } else if (isDiagonal()) {
+            val a = min(start.first, end.first)
+            val b = max(start.first, end.first)
+
+            val c = min(start.second, end.second)
+            val d = max(start.second, end.second)
+
+            val flipped = (start.first == a && start.second == d || start.first == b && start.second == c)
+
+            val rangeX = (a..b).toList()
+            val rangeY = if (flipped) (c..d).toList().reversed() else (c..d).toList()
+
+            return rangeX.zip(rangeY) { x, y -> Point(x, y) }
+
         } else {
             TODO()
         }
     }
 }
 
-operator fun <T> List<T>.component11() = this[10]
+
 
 fun main() {
-
     fun part1(input: List<Line>, size: Int): Int {
         val perps = input.filter { it.isHorizontal() || it.isVertical() }
 
         val plane = Array(size) { IntArray(size) }
 
         perps.forEach { line->
-           line.getLatticePoints().forEach { point ->
-               plane[point.first][point.second] += 1
-           }
+            line.getLatticePoints().forEach { point ->
+                plane[point.first][point.second] += 1
+            }
+        }
+
+        val countMultipleHits = plane
+            .map { column -> column.count {it > 1} }
+            .reduce { sum, rowCount -> sum + rowCount }
+
+        return countMultipleHits
+    }
+
+    fun part2(input: List<Line>, size: Int): Int {
+        val perps = input.filter { it.isHorizontal() || it.isVertical() || it.isDiagonal() }
+
+        val plane = Array(size) { IntArray(size) }
+
+        perps.forEach { line->
+            line.getLatticePoints().forEach { point ->
+                plane[point.first][point.second] += 1
+            }
         }
 
         val countMultipleHits = plane
@@ -71,8 +109,8 @@ fun main() {
 
     check(minCoord >= 0)
 
-    println("part1 = ${part1(testInput, maxCoord+1)}")
-    //println("part2 = ${part2(guessed, boards)}")
+    println("part1 = ${part1(testInput, maxCoord + 1)}")
+    println("part2 = ${part2(testInput, maxCoord + 1)}")
 
     return
 }
