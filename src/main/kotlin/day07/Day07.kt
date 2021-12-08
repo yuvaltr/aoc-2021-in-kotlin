2a4b1f07b77ead93e66bbe7d7588efe2948a4476
Yuval Traitelis <yuval.traitelis@foretellix.com>
2021-12-08 11:38:59 +0200

    day 7 - fuel cost

---
+++ src/main/kotlin/day07/Day07.kt
@@ -0,0 +1,48 @@
+package day07
+
+import readInput
+import java.lang.Math.abs
+import java.util.*
+
+fun part1(displacements: List<Int>): Int {
+    val totalFuel = HashMap<Int, Int>()
+    var minFuel = Int.MAX_VALUE
+
+    displacements.forEach { coord ->
+        if (!totalFuel.contains(coord)) {
+            totalFuel[coord] = displacements.map { abs(it - coord) }.reduce { acc, i -> acc + i }
+            if (totalFuel[coord]!! < minFuel)
+                minFuel = totalFuel[coord]!!
+        }
+    }
+
+    return minFuel
+}
+
+fun part2(displacements: List<Int>): Int {
+    val totalFuel = HashMap<Int, Int>()
+    var minFuel = Int.MAX_VALUE
+
+    val a = displacements.minOrNull()!!
+    val b = displacements.maxOrNull()!!
+
+    (a..b).forEach { coord ->
+        if (!totalFuel.contains(coord)) {
+            totalFuel[coord] = displacements.map { val d = abs(it - coord); ((d+1)*d)/2 }.reduce { acc, i -> acc + i }
+            if (totalFuel[coord]!! < minFuel)
+                minFuel = totalFuel[coord]!!
+        }
+    }
+
+    return minFuel
+}
+
+fun main() {
+
+    val testInput = readInput("day07/input.txt").first().split(",").map { it.toInt() }
+
+    println("part1 = ${part1(testInput)}")
+    println("part2 = ${part2(testInput)}")
+
+    return
+}
