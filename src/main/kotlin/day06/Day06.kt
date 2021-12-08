3229fb7e28abfe20662e39f3ef9362711492088b
Yuval Traitelis <yuval.traitelis@foretellix.com>
2021-12-08 10:38:47 +0200

    day 6 - count fish

---
+++ src/main/kotlin/day06/Day06.kt
@@ -0,0 +1,44 @@
+package day06
+
+import readInput
+import java.util.*
+
+fun part1(input: List<Int>, days: Int): Int {
+    var state = input.toMutableList()
+
+    for (i in 1..days) {
+        val newState = state.map{ it - 1 }
+        val hatched = newState.count { it < 0 }
+        state = newState.map { if (it < 0) 6 else it }.toMutableList()
+        state.addAll(List(hatched) { 8 })
+    }
+
+    return state.size
+}
+
+fun part2(input: List<Int>, days: Int): Long {
+    var count = LongArray(size = 9)
+    for (age in 0..8) {
+        count[age] = input.count { it == age }.toLong()
+    }
+
+    for (i in 1..days) {
+        val newState = count.toList()
+        Collections.rotate(newState, -1)
+        val hatched = newState[8]
+        count = newState.toLongArray()
+        count[6] += hatched
+    }
+
+    return count.reduce { acc, i -> acc + i }
+}
+
+fun main() {
+
+    val testInput = readInput("day06/input.txt").first().split(",").map { it.toInt() }
+
+    println("part1 = ${part1(testInput, days = 80)}")
+    println("part2 = ${part2(testInput, days = 256)}")
+
+    return
+}
