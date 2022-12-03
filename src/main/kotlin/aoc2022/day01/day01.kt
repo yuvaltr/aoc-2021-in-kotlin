202a3902ee90b88720aece7e6def4fcfbc64332b
Yuval Traitelis <yuvaltr@gmail.com>
2022-12-04 01:28:54 +0200

    AoC 2022 day 1, 2 & 3 done

--- src/main/kotlin/aoc2022/day01/day01.kt
+++ src/main/kotlin/aoc2022/day01/day01.kt
@@ -2,25 +2,28 @@ package aoc2022.day01
 
 import readInput
 
-
-fun main() {
-    val input = readInput("aoc2022/day01/input.txt").map { it.toIntOrNull() ?: 0 }
-
-    val result = input
+fun <T> List<T>.splitBy(delimiter: T): List<List<T>> {
+    return this
         .flatMapIndexed { index, x ->
             when {
-                index == 0 || index == input.lastIndex -> listOf(index)
-                x == 0 -> listOf(index - 1, index + 1)
+                index == 0 || index == this.lastIndex -> listOf(index)
+                x == delimiter -> listOf(index - 1, index + 1)
                 else -> emptyList()
             }
         }
-        .windowed(size = 2, step = 2) { (from, to) -> input.slice(from..to) }
+        .windowed(size = 2, step = 2) { (from, to) -> this.slice(from..to) }
+}
 
-    val sums = result.map { list -> list.sumOf { it } }
+fun main() {
+    val numbers = readInput("aoc2022/day01/input.txt")
+        .map { it.toIntOrNull() ?: 0 }
+        .splitBy(0)
+
+    val sums = numbers.map { list -> list.sumOf { it } }
 
     val max = sums.maxOf { it }
+    println("part1 = $max")
 
     val maxThree = sums.sortedDescending().take(3).sumOf { it }
-
-    return
+    println("part2 = $maxThree")
 }
